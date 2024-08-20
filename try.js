c6f08ad7bb3d8138352e00469067526a2587de36
joet18 <j.lil.boy116@gmail.com>
2024-08-20 22:00:20 +0300

    Changes to Java

---
+++ try.js
@@ -0,0 +1,21 @@
+ console.log("Hi jossy");
+ //now we use arthimetic operations//
+ console.log(10*4+8-6);
+ console.log("hello"+5*10);
+ const haiku = "Blowing from the west"+"\nFallen leaves gather"+"\nIn the east."
+ console.log(haiku);
+    let thingone="how are you!";
+    let thingtwo="Joseph";
+    console.log(thingone +""+thingtwo);
+    const fullname="Joseph.T";
+    console.log(fullname);
+let bill=10.25 + 3.99 + 7.15;
+    let tip= 0.15*bill;
+    let total= bill + tip;
+  console.log(total);    
+  const adjective1 = "amazing";
+  const adjective2 = "fun";
+  const adjective3 = "entertaining";
+     const mint="The Intro to JavaScript course is "+adjective1+". \nJames and Julia areso"+adjective2+". \nI cannot wait to work through the rest of this "+adjective3+" content!";
+console.log(mint);
+
